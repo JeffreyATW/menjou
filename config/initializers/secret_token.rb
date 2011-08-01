f580cf0a784b582ead2e893cb0dd9d404a1241fc
Jeffrey Carl Faden <jeffreyatw@gmail.com>
2011-08-01 14:24:46 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Menjou::Application.config.secret_token = '48b2292f0707604d041ed983b604a4dc5d3f876e3da9abb4391f1d7958168e9a9de830f1c852993715408566652bb808255dd7af628e30b377e4668db0495683'
